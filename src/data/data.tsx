a0db96c9c8154effef3f56eb4bdf63155e98c5d2
patrykrosenkiewicz <patrykrosenkiewicz@gmail.com>
2023-10-01 15:31:00 +0200

    filled personal data

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -44,7 +44,7 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
+  title: 'Patryk Rosenkiewicz',
   description: "Example site built with Tim Baker's react resume template",
 };
 
@@ -69,18 +69,17 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `I'm Patryk Rosenkiewicz.`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I'm a <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
+        at <strong className="text-stone-100">The Software House</strong> helping with realization of customers plans.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        In my free time time, you can catch me <strong className="text-stone-100">riding motorcycle</strong>,
+        walking my <strong className="text-stone-100">dog</strong>, or playing with{' '}
+        <strong className="text-stone-100">new technologies</strong>.
       </p>
     </>
   ),
@@ -103,17 +102,14 @@ export const heroData: Hero = {
  * About section
  */
 export const aboutData: About = {
-  profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description: `Web developer with 5 years of experience. I worked on a great variety of projects written in Perl, Bash, C++, Ruby, PHP, Python, and JavaScript. Love learning new things and playing with my two dogs.`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
+    {label: 'Location', text: 'Gdańsk', Icon: MapIcon},
     {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
+    {label: 'Nationality', text: 'Polish', Icon: FlagIcon},
+    {label: 'Interests', text: 'Psychology, dogs, new technologies', Icon: SparklesIcon},
+    {label: 'Study', text: 'University of Gdańsk, Psychology', Icon: AcademicCapIcon},
+    {label: 'Employment', text: 'The Software House', Icon: BuildingOffice2Icon},
   ],
 };
 
@@ -125,16 +121,12 @@ export const skills: SkillGroup[] = [
     name: 'Spoken languages',
     skills: [
       {
-        name: 'English',
+        name: 'Polish',
         level: 10,
       },
       {
-        name: 'French',
-        level: 4,
-      },
-      {
-        name: 'Spanish',
-        level: 3,
+        name: 'English',
+        level: 8,
       },
     ],
   },
@@ -143,15 +135,19 @@ export const skills: SkillGroup[] = [
     skills: [
       {
         name: 'React',
-        level: 9,
+        level: 6,
       },
       {
         name: 'Typescript',
-        level: 7,
+        level: 8,
       },
       {
         name: 'GraphQL',
-        level: 6,
+        level: 9,
+      }, 
+      {
+        name: 'NextJS',
+        level: 5,
       },
     ],
   },
@@ -163,103 +159,47 @@ export const skills: SkillGroup[] = [
         level: 8,
       },
       {
-        name: 'Rust',
+        name: 'PostgreSQL',
+        level: 8,
+      },
+      {
+        name: 'Perl',
+        level: 6,
+      },
+      {
+        name: 'PHP',
+        level: 4,
+      },
+    ],
+  },
+  {
+    name: 'DevOps',
+    skills: [
+      {
+        name: 'GitHub Actions',
+        level: 6,
+      },
+      {
+        name: 'Docker',
+        level: 7,
+      },
+      {
+        name: 'VirtualBox',
         level: 5,
       },
       {
-        name: 'Golang',
+        name: 'AWS / Azure',
+        level: 5,
+      },
+      {
+        name: 'Linux',
+        level: 7,
+      },
+      {
+        name: 'Bash',
         level: 4,
       },
     ],
-  },
-  {
-    name: 'Mobile development',
-    skills: [
-      {
-        name: 'React Native',
-        level: 9,
-      },
-      {
-        name: 'Flutter',
-        level: 4,
-      },
-      {
-        name: 'Swift',
-        level: 3,
-      },
-    ],
-  },
-];
-
-/**
- * Portfolio section
- */
-export const portfolioItems: PortfolioItem[] = [
-  {
-    title: 'Project title 1',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage1,
-  },
-  {
-    title: 'Project title 2',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage2,
-  },
-  {
-    title: 'Project title 3',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage3,
-  },
-  {
-    title: 'Project title 4',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage4,
-  },
-  {
-    title: 'Project title 5',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage5,
-  },
-  {
-    title: 'Project title 6',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage6,
-  },
-  {
-    title: 'Project title 7',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage7,
-  },
-  {
-    title: 'Project title 8',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage8,
-  },
-  {
-    title: 'Project title 9',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage9,
-  },
-  {
-    title: 'Project title 10',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage10,
-  },
-  {
-    title: 'Project title 11',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage11,
   },
 ];
 
@@ -268,39 +208,51 @@ export const portfolioItems: PortfolioItem[] = [
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
+    date: 'October 2020',
+    location: 'University of Gdańsk',
+    title: 'Masters in Psychology',
     content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
-  },
-  {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
-  },
+  }
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    date: 'August 2023 - Present',
+    location: 'The Software House',
+    title: 'Node.js developer',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        Backend developer in greenfield project which is responsble for comparing banking products. Main technologies: TypeORM, Express.js, TypeScript and PostgreSQL.
       </p>
     ),
   },
   {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
+    date: 'May 2023 - July 2023',
+    location: 'The Software House',
+    title: 'Full-stack developer',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        Data warehouse hosted on AWS cloud. I was responsible for adjusting the process of authentication by omitting a proxy module responsible for integrating with the self-hosted IAM service, keycloak. I also did minor improvements and optimizations of data imports within step-functions workflows. Main technologies: TypeScript, AWS Step functions, S3, PostgreSQL and Keycloak.
+      </p>
+    ),
+  },
+  {
+    date: 'April 2022 - May 2023',
+    location: 'The Software House',
+    title: 'Full-stack developer',
+    content: (
+      <p>
+        Legacy CRM application which main features was to create contracts and then generate invoices. I was responsible for refactoring the code and adding new features. Application had plenty of integrations with external services. And application used self-hosted IAM service, FusionAuth for authorization and authentication. Main technologies: Node.js, Express.js, TypeScript, PostgreSQL, FusionAuth, React and Next.js.
+      </p>
+    ),
+  },
+  {
+    date: 'January 2018 - March 2022',
+    location: 'DIR',
+    title: 'Full-stack developer',
+    content: (
+      <p>
+        Responsible for developing and maintaining stack of applications related to call centers. Main features of applications was to manage call center, create new agents, queues setting priorites etc. And calculate statistics of call centers like eg. call durations, call hangout reason, response time etc. Main technologies: Perl, PHP, JavaScript, JQuery, Bash and PostgreSQL.
       </p>
     ),
   },
@@ -336,27 +288,22 @@ export const testimonial: TestimonialSection = {
 
 export const contact: ContactSection = {
   headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  description: 'Feel free to send me an email or reach me through linkedIn.',
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      text: 'patrykrosenkiewicz@gmail.com',
+      href: 'mailto:patrykrosenkiewicz@gmail.com',
     },
     {
       type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
-    },
-    {
-      type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
+      text: 'Gdańsk, Poland',
+      href: 'https://www.google.pl/maps/place/Gda%C5%84sk/@54.3611752,18.5252233,11z',
     },
     {
       type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      text: 'patrykrosenkiewicz',
+      href: 'https://github.com/patrykrosenkiewicz',
     },
   ],
 };
@@ -365,9 +312,6 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/reactresume/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/patrykrosenkiewicz'},
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://pl.linkedin.com/in/patryk-rosenkiewicz-5751b5130'},
 ];
