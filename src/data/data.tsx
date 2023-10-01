d02a7fbaad17336c4149b8698d630433c8bc60a8
patrykrosenkiewicz <patrykrosenkiewicz@gmail.com>
2023-10-01 15:35:43 +0200

    fix nav

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -55,11 +55,9 @@ export const SectionId = {
   Hero: 'hero',
   About: 'about',
   Contact: 'contact',
-  Portfolio: 'portfolio',
   Resume: 'resume',
   Skills: 'skills',
   Stats: 'stats',
-  Testimonials: 'testimonials',
 } as const;
 
 export type SectionId = (typeof SectionId)[keyof typeof SectionId];
